0582762fd0d24773e4eef4952d93ce2657135bee
Marion904 <marionfontaine904@gmail.com>
2018-02-21 16:19:56 +0100

    One change

--- src/drivemigs/servlet/com/SignUp.java
+++ src/drivemigs/servlet/com/SignUp.java
@@ -40,6 +40,7 @@ public class SignUp extends HttpServlet {
 	public static final String ERROR_UN2="Merci de renseigner ton nom<br>";
 	public static final String ERROR_FN1="Le prénom est trop courts<br>";
 	public static final String ERROR_FN2="Merci de renseigner ton prénom<br>";
+	public static final String ERROR_MAIL1="Veuillez saisir une adresse mail valide<br>";
 //	public HashMap<String,User> users = new HashMap<String,User>();
 	
     /**
@@ -156,7 +157,7 @@ public class SignUp extends HttpServlet {
 		private String validateEmail(String email) {
 			if( email != null && email.trim().length()!=0) {
 				if ( !email.matches( "([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)" ) ) {
-					return("Veuillez saisir une adresse mail valide<br>");
+					return(ERROR_MAIL1);
 				}else {
 					return null;
 				}
